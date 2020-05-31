91ddb98eee60524c07ffd9b9950623872917806a
m-lima <marcelowind@gmail.com>
2020-05-31 12:33:05 +0000

    Allow configuring form command-line

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,62 @@
-#[derive(Debug)]
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(name = "Simple Server", about = "A simple multi-purpose server")]
+struct RawOptions {
+    /// Port that the the server should bind to
+    #[clap(short, long, default_value = "3030")]
+    port: u16,
+
+    /// Serve from filesystem path
+    #[clap(short, long)]
+    file: Vec<RawPathAction<std::path::PathBuf>>,
+
+    /// Respond with a redirect
+    #[clap(short, long)]
+    redirect: Vec<RawPathAction<warp::http::Uri>>,
+
+    /// Respond with status code
+    #[clap(short, long)]
+    status: Vec<RawPathAction<warp::http::StatusCode>>,
+}
+
 pub struct Options {
     pub port: u16,
     pub paths: std::collections::HashSet<PathAction>,
+}
+
+impl Options {
+    pub fn init() -> Self {
+        let raw_options: RawOptions = RawOptions::parse();
+        let mut paths = std::collections::HashSet::with_capacity(
+            raw_options.file.len() + raw_options.redirect.len() + raw_options.status.len(),
+        );
+        raw_options
+            .file
+            .into_iter()
+            .map(RawPathAction::to_path_action)
+            .for_each(|p| {
+                paths.insert(p);
+            });
+        raw_options
+            .redirect
+            .into_iter()
+            .map(RawPathAction::to_path_action)
+            .for_each(|p| {
+                paths.insert(p);
+            });
+        raw_options
+            .status
+            .into_iter()
+            .map(RawPathAction::to_path_action)
+            .for_each(|p| {
+                paths.insert(p);
+            });
+        Self {
+            port: raw_options.port,
+            paths,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -63,6 +118,98 @@ impl std::fmt::Display for Action {
             Action::ServePath(path) => write!(fmt, "Serving {}", path.display()),
             Action::Redirect(uri) => write!(fmt, "Redirecting to {}", uri),
             Action::StatusCode(status) => write!(fmt, "Responding {}", status),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct RawPathAction<A>
+where
+    A: RawAction,
+{
+    path: Path,
+    action: A,
+}
+
+impl<A> RawPathAction<A>
+where
+    A: RawAction,
+{
+    fn to_path_action(self) -> PathAction {
+        PathAction {
+            path: self.path,
+            action: self.action.to_action(),
+        }
+    }
+}
+
+impl<A> std::str::FromStr for RawPathAction<A>
+where
+    A: RawAction,
+{
+    type Err = ArgError;
+
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        let parts = string
+            .find(':')
+            .map(|index| (Path::from(&string[..index]), &string[index + 1..]))
+            .ok_or(ArgError::Format)?;
+        let action = A::from_str(parts.1).map_err(|_| ArgError::Convert)?;
+        if action.valid() {
+            Ok(Self {
+                path: parts.0,
+                action,
+            })
+        } else {
+            Err(ArgError::Invalid(parts.1.to_string()))
+        }
+    }
+}
+
+trait RawAction: std::str::FromStr {
+    fn to_action(self) -> Action;
+    fn valid(&self) -> bool {
+        true
+    }
+}
+
+impl RawAction for std::path::PathBuf {
+    fn to_action(self) -> Action {
+        Action::ServePath(self)
+    }
+
+    fn valid(&self) -> bool {
+        self.exists()
+    }
+}
+
+impl RawAction for warp::http::Uri {
+    fn to_action(self) -> Action {
+        Action::Redirect(self)
+    }
+}
+
+impl RawAction for warp::http::StatusCode {
+    fn to_action(self) -> Action {
+        Action::StatusCode(self)
+    }
+}
+
+#[derive(Debug)]
+pub enum ArgError {
+    Format,
+    Convert,
+    Invalid(String),
+}
+
+impl std::error::Error for ArgError {}
+
+impl std::fmt::Display for ArgError {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ArgError::Format => write!(fmt, "Expected format [URL_PATH]:<VALUE>"),
+            ArgError::Convert => write!(fmt, "Failed to convert value"),
+            ArgError::Invalid(value) => write!(fmt, "Value passed is not valid: {}", value),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -93,68 +93,12 @@ fn to_filter(path_action: config::PathAction) -> warp::filters::BoxedFilter<(Box
 
 #[tokio::main]
 async fn main() {
-    use std::iter::FromIterator;
+    let options = config::Options::init();
 
-    let path = std::env::args().nth(1).map_or_else(
-        || {
-            eprintln!("No path provided");
-            std::process::exit(-1);
-        },
-        |path_string| {
-            let path = std::path::PathBuf::from(path_string.clone());
-
-            if !path.exists() {
-                eprintln!("Path provided does not exist: {}", &path_string);
-                std::process::exit(-1);
-            }
-
-            if !path.is_dir() {
-                eprintln!("Path provided is not a directory: {}", &path_string);
-                std::process::exit(-1);
-            }
-
-            path
-        },
-    );
-
-    let port = std::env::args()
-        .nth(2)
-        .map_or(Ok(3030_u16), |arg| arg.parse::<u16>())
-        .unwrap_or_else(|err| {
-            eprintln!("Invalid port: {}", err);
-            std::process::exit(-1);
-        });
-
-    let options = config::Options {
-        port,
-        paths: std::collections::HashSet::from_iter(
-            vec![
-                config::PathAction {
-                    path: config::Path::from("bla"),
-                    action: config::Action::ServePath(path.clone()),
-                },
-                config::PathAction {
-                    path: config::Path::from("ble"),
-                    action: config::Action::Redirect(warp::http::Uri::from_static(
-                        "http://www.google.com",
-                    )),
-                },
-                config::PathAction {
-                    path: config::Path::from("bli"),
-                    action: config::Action::StatusCode(
-                        warp::http::StatusCode::from_u16(550).unwrap(),
-                    ),
-                },
-                config::PathAction {
-                    path: config::Path::from("bli/bla"),
-                    action: config::Action::StatusCode(
-                        warp::http::StatusCode::from_u16(555).unwrap(),
-                    ),
-                },
-            ]
-            .into_iter(),
-        ),
-    };
+    if options.paths.is_empty() {
+        eprintln!("No routes provided");
+        std::process::exit(-1);
+    }
 
     print_options(&options);
 
